6a6e790280ab2bf7dd85c19827299bdc413e042b
cceph <yousefelsisyy@gmail.com>
2023-02-27 18:06:15 -0400

    Create homepage using JS

--- src/homepage.js
+++ src/homepage.js
@@ -1,3 +1,5 @@
+import steak from "./resources/steak-feast.jpg";
+
 // Bind DOM
 const content = document.querySelector("#content");
 
@@ -47,9 +49,59 @@ function createHeader() {
   return header;
 }
 
-function createMain() {}
+function createBanner() {
+  const banner = document.createElement("div");
+  banner.classList.add("banner");
 
-function createFooter() {}
+  const steakImg = new Image();
+  steakImg.src = steak;
+  banner.appendChild(steakImg);
+
+  return banner;
+}
+
+function createIntro() {
+  const intro = document.createElement("div");
+  intro.classList.add("intro");
+
+  const paragraph = document.createElement("p");
+  paragraph.textContent = `This restaurant is amazing. Lorem ipsum dolor sit amet,
+    consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Cursus mattis molestie a iaculis at
+    erat. Sed tempus urna et pharetra. Laoreet non curabitur gravida
+    arcu ac. Mi ipsum faucibus vitae aliquet nec ullamcorper.
+    Consequat nisl vel pretium lectus quam id leo. Tincidunt lobortis
+    feugiat vivamus at. Accumsan lacus vel facilisis volutpat est
+    velit. Tortor consequat id porta nibh venenatis cras sed felis
+    eget. Urna nunc id cursus metus aliquam.`;
+  intro.appendChild(paragraph);
+
+  return intro;
+}
+
+function createBodyMain() {
+  const bodyMain = document.createElement("div");
+  bodyMain.classList.add("main");
+
+  const banner = createBanner();
+  bodyMain.appendChild(banner);
+
+  const intro = createIntro();
+  bodyMain.appendChild(intro);
+
+  return bodyMain;
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const text = document.createElement("p");
+  text.textContent = "Copyright of CCeph";
+  footer.appendChild(text);
+
+  return footer;
+}
 
 function createMainGrid() {
   const mainGrid = document.createElement("div");
@@ -58,11 +110,11 @@ function createMainGrid() {
   const header = createHeader();
   mainGrid.appendChild(header);
 
-  const main = createMain();
-  // Append
+  const bodyMain = createBodyMain();
+  mainGrid.appendChild(bodyMain);
 
   const footer = createFooter();
-  // Append
+  mainGrid.appendChild(footer);
 
   return mainGrid;
 }
