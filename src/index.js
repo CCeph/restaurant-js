c39ef4a5ec2591e73b0021253a60f575093b99f8
cceph <yousefelsisyy@gmail.com>
2023-02-27 12:31:38 -0400

    Add a picture as a banner to homepage

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import "./style.css";
+import restaurant from "./resources/steak-feast.jpg";
+
+const pic = new Image();
+pic.src = restaurant;
+
+const container = document.querySelector(".banner");
+container.appendChild(pic);
 
 console.log("Webpack works");
