3bad1431496794d5646c8176a25e4fd39ff9802e
Yousef <yousefelsisyy@gmail.com>
2023-03-03 18:17:28 -0400

    Add a function to create dishes on menu
    Two dishes have been added to the menu using this function.

--- src/menu.js
+++ src/menu.js
@@ -1,8 +1,42 @@
 import { createHeader, createFooter, content } from "./homepage";
+import steakFeast from "./resources/steak-feast.jpg";
+import salmonImg from "./resources/salmon-dish.jpg";
+import burgerImg from "./resources/burger2.jpg";
+
+function createDish(name, imageSrc) {
+  const foodContainer = document.createElement("div");
+  foodContainer.classList.add("food-container");
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+  foodContainer.appendChild(imageContainer);
+
+  const image = new Image();
+  image.src = imageSrc;
+  imageContainer.appendChild(image);
+
+  const foodTitle = document.createElement("h3");
+  foodTitle.textContent = name;
+  foodContainer.appendChild(foodTitle);
+
+  const foodText = document.createElement("p");
+  foodText.textContent = `Lorem ipsum dolor sit amet, 
+  consectetur adipiscing elit, sed do eiusmod tempor incididunt
+  ut labore et dolore magna aliqua.`;
+  foodContainer.appendChild(foodText);
+
+  return foodContainer;
+}
 
 function createBodyMain() {
   const body = document.createElement("div");
-  body.classList.add("main");
+  body.classList.add("menu-grid");
+
+  const firstItem = createDish("Steak Feast", steakFeast);
+  body.appendChild(firstItem);
+
+  const secondItem = createDish("Burger", burgerImg);
+  body.appendChild(secondItem);
 
   return body;
 }
